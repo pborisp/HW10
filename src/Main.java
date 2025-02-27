c29062dc41f7d4124dd4033357c1337fbb9b8f28
pborisp <popovbs@gmail.com>
2025-02-27 21:58:49 +0300

    Task 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Main {
 
     public static void getYear(int meansYear) {
@@ -8,10 +11,29 @@ public class Main {
         }
     }
 
+    public static void getOS(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 1 && clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear == currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
     public static void main(String[] args) {
         //Задача №1
         System.out.println("Задача №1:");
         int year = 2024;
         getYear(year);
+
+        //Задача №2
+        System.out.println("Задача №2:");
+        int deviceYear = 2025;
+        int OS = 0;
+        getOS(OS, deviceYear);
     }
 }
