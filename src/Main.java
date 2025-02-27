39127d2576c6bba818f9352187cc9a97586ad710
pborisp <popovbs@gmail.com>
2025-02-27 21:45:54 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void getYear(int meansYear) {
+        if ((meansYear % 400) == 0 || (meansYear % 4 == 0 && meansYear % 100 != 0)) {
+            System.out.println(meansYear + " год является високосным");
+        } else {
+            System.out.println(meansYear + " год не является високосным");
+        }
+    }
+
+    public static void main(String[] args) {
+        //Задача №1
+        System.out.println("Задача №1:");
+        int year = 2024;
+        getYear(year);
     }
 }
